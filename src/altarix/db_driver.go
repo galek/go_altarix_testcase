c9e61d39b6b141e3872a649b794056b652b0d7c8
Nick Galko <nikolay.galko@gmail.com>
2017-09-10 20:34:02 +0400

    WIP under selection of object by access_token

--- src/altarix/db_driver.go
+++ src/altarix/db_driver.go
@@ -3,48 +3,97 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"log"
+
 	_ "github.com/lib/pq"
 )
 
-const DB_CONNECT_STRING = 
-"host=localhost port=5432 user=postgres password=postgres dbname=altarix sslmode=disable"
+/*
+https://github.com/go-pg/pg
+https://github.com/lib/pq
+https://github.com/lib/pq/blob/master/listen_example/doc.go
+*/
+
+const DB_CONNECT_STRING = "host=localhost port=5432 user=postgres password=postgres dbname=altarix sslmode=disable"
+
+var DB *sql.DB
+
+func OpenConnectionToDB() {
+	DB, err = sql.Open("postgres", DB_CONNECT_STRING)
+	if err != nil {
+		log.Println("Database opening error -->%v\n", err)
+		panic("Database error")
+	}
+
+	printError(file_line())
+}
+
+func CloseConnectionToDB() {
+	defer DB.Close()
+}
 
 func connectionToDB() {
-	db, err := sql.Open("postgres", DB_CONNECT_STRING)
-    defer db.Close()
+	OpenConnectionToDB()
+	// init_database(&db)
+	GetUIDFromAccessTokensByToken("'38b2cfb8-eb40-fc3d-9a81-49304b21cdb6'", &DB)
+	//CloseConnectionToDB()
+}
 
-    if err != nil {
-        fmt.Printf("Database opening error -->%v\n", err)
-        panic("Database error")
-	}
-	
-	init_database(&db)
+func GetUIDFromAccessTokensByToken(_token string, pdb **sql.DB) {
+	db := *pdb
+	var req string = "SELECT uid FROM access_tokens WHERE token = ?"
+	var stntMessageBody *sql.Stmt
+	stntMessageBody, err = db.Prepare(req)
+
+	printError(file_line())
+
+	println("token: %s", _token)
+
+	fmt.Println("finished")
+
+	stntMessageBody.Query(_token)
+
+	//Читаем все значения
+	// var rows *sql.Rows
+	// rows, err = stntMessageBody.Query(_token)
+
+	// var UID int
+
+	// for rows.Next() {
+	// 	rows.Scan(&UID)
+	// 	log.Println("[DEBUG ONLY] %s %i", _token, UID)
+	// }
+	// printError(file_line())
+
+	// defer rows.Close()
+	// defer stntMessageBody.Close()
+	// return 0;
 }
 
 /*-----------------------------------------------------------------------------*/
 func init_database(pdb **sql.DB) {
-	
-		db := *pdb
-	
-		init_db_strings := []string{
-			"DROP SCHEMA IF EXISTS sb CASCADE;",
-			"CREATE SCHEMA sb;",
-			//be careful - next multiline string is quoted by backquote symbol
-			`CREATE TABLE sb.test_data(
-			 id serial,
-			 device_id integer not null,
-			 parameter_id integer not null,
-			 value varchar(100),
-			 event_ctime timestamp default current_timestamp,
-			 constraint id_pk primary key (id));`}
-	
-		for _, qstr := range init_db_strings {
-			_, err := db.Exec(qstr)
-	
-			if err != nil {
-				fmt.Printf("Database init error -->%v\n", err)
-				panic("Query error")
-			}
-		}
-		fmt.Println("Database rebuilded successfully")
-	}
+
+	// db := *pdb
+
+	// init_db_strings := []string{
+	// 	"DROP SCHEMA IF EXISTS sb CASCADE;",
+	// 	"CREATE SCHEMA sb;",
+	// 	//be careful - next multiline string is quoted by backquote symbol
+	// 	`CREATE TABLE sb.test_data(
+	// 		 id serial,
+	// 		 device_id integer not null,
+	// 		 parameter_id integer not null,
+	// 		 value varchar(100),
+	// 		 event_ctime timestamp default current_timestamp,
+	// 		 constraint id_pk primary key (id));`}
+
+	// for _, qstr := range init_db_strings {
+	// 	_, err := db.Exec(qstr)
+
+	// 	if err != nil {
+	// 		fmt.Printf("Database init error -->%v\n", err)
+	// 		panic("Query error")
+	// 	}
+	// }
+	// fmt.Println("Database rebuilded successfully")
+}
